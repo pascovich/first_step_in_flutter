a076ca4572046ef34a4dba2c0f63f327c174bd3e
pascovich <kaluzipascovich@gmail.com>
2024-09-27 10:54:41 +0200

    drawer menu okay

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MaterialApp(
     theme: ThemeData(colorSchemeSeed: Colors.blue),
     home: const HomePage(),
+    // home: const HomePage(),
     debugShowCheckedModeBanner: false,
   ));
 }
@@ -22,12 +23,49 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            leading: const Icon(Icons.menu),
-            title: const Center(child: Text("E-Booking")),
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            // foregroundColor: Colors.pinkAccent,
+            // backgroundColor: Colors.pinkAccent,
+            // leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+            title: const Text("E-Booking"),
+            // title: const Center(child: Text("E-Booking")),
             elevation: 10,
             actions: [
-              IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
             ]),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                child: Text("Menu"),
+                // decoration: BoxDecoration(color: Colors.blue),
+              ),
+              ListTile(
+                leading: const Icon(Icons.account_box),
+                title: const Text("Profile"),
+                subtitle: const Text("Mes informations"),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.help),
+                title: const Text("Help"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text("Parametres"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text("Logout"),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
         body: const Center(child: Text("menus fluterdd")),
         bottomNavigationBar: Container(
             decoration: const BoxDecoration(
@@ -48,9 +86,13 @@ class HomePageState extends State<HomePage> {
                       icon: Icon(Icons.book_rounded),
                       label: "Mes reservations"),
                   NavigationDestination(
-                      icon: Icon(Icons.person), label: "Profile"),
+                      icon: Icon(Icons.account_box), label: "Profile"),
                   NavigationDestination(
                       icon: Icon(Icons.phone), label: "Appels"),
-                ])));
+                ])),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ));
   }
 }
