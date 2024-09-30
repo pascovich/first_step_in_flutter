7d9b6d14e925c1c2042c26d1c50dcc4a385cd430
pascovich <kaluzipascovich@gmail.com>
2024-09-30 15:51:31 +0200

    event page(Hero banner)

--- lib/components/Hero_widget.dart
+++ lib/components/Hero_widget.dart
@@ -8,12 +8,98 @@ class HeroWidget extends StatefulWidget {
 }
 
 class _HeroWidgetState extends State<HeroWidget> {
+  final _infos = [
+    {
+      'code': 'Voyage au kenya',
+      'title': 'maked your online jorney',
+      'image':
+          'https://th.bing.com/th/id/R.da9b5dbb601dec1a5060909fceff588f?rik=pu6oJO39PEE%2bRQ&pid=ImgRaw&r=0'
+    },
+    {
+      'code': 'Go away from',
+      'title': 'in your make dvshd nuorfer',
+      'image':
+          'https://pixnio.com/free-images/2017/10/25/2017-10-25-14-43-24.jpg'
+    },
+    {
+      'code': 'Journey in Paris',
+      'title': 'pour tout vos paris spoartif',
+      'image':
+          'https://th.bing.com/th/id/R.e8409546bb2f8dffaf5e6c27c106d976?rik=0iylvn%2flxrwIng&riu=http%3a%2f%2fimg.20mn.fr%2f8yVNHz5SR42-wbLyndTHdA%2f2048x1536-fit_vue-ensemble-ville-paris.jpg&ehk=jhk17YiHAppELUoECyR9RvbTLATEVsFPCS0c8J%2fIp78%3d&risl=&pid=ImgRaw&r=0'
+    }
+  ];
+  int _currentInfo = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 200,
+      width: double.infinity,
       decoration: BoxDecoration(
-          color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+          color: Colors.blue, borderRadius: BorderRadius.circular(19)),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            padding: EdgeInsets.only(bottom: 17),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: _infos.map((infos) => _buildPagination(infos)).toList(),
+            ),
+          ),
+          Positioned(
+              top: 20,
+              left: 20,
+              child: Text(_infos[_currentInfo]['code']!.toUpperCase(),
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold))),
+          Positioned(
+              top: 60,
+              left: 20,
+              child: Text(_infos[_currentInfo]['title']!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ))),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPagination(Map<String, String> info) {
+    return Container(
+      width: _infos.indexOf(info) == _currentInfo ? 30 : 10,
+      height: 10,
+      margin: const EdgeInsets.symmetric(horizontal: 3),
+      decoration: BoxDecoration(
+        color: _infos.indexOf(info) == _currentInfo
+            ? Colors.white
+            : Colors.white54,
+        // color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      // children: [
+      //   Image.network(infos['image']),
+      //   Positioned(
+      //     bottom: 12,
+      //     right: 12,
+      //     child: Text(
+      //       infos['title'],
+      //       style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+      //     ),
+      //   )
+      // ],
+      // alignment: Alignment.bottomRight,
+      // child: Container(
+      //   padding: EdgeInsets.symmetric(horizontal: 12),
+      //   child: Text(
+      //     infos['code'],
+      //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      //   ),
+      // ),
     );
   }
 }
